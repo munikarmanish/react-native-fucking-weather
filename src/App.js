e92e2fc130c518981728d38115b099c9694d38b2
Manish Munikar <munikarmanish@gmail.com>
2017-11-18 18:06:02 +0545

    eslint + basic styling

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
