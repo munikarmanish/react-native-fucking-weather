3d786e2911758799ee6e79dbe9e1be0dc9a90184
Manish Munikar <munikarmanish@gmail.com>
2017-11-18 18:28:00 +0545

    more styling

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello</Text>
+        <View style={styles.header}>
+          <Text style={styles.icon}>ICON</Text>
+          <Text style={styles.temperature}>24°</Text>
+        </View>
+        <View style={styles.body}>
+          <Text style={styles.title}>Build a Fucking Weather App</Text>
+          <Text style={styles.subtitle}>Let's make it rain</Text>
+        </View>
       </View>
     );
   }
@@ -14,9 +21,40 @@ class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'yellow',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    backgroundColor: '#efc017',
+  },
+  header: {
+    flex: 1,
+    flexDirection: 'row',
+    // backgroundColor: 'red',
+    justifyContent: 'space-around',
     alignItems: 'center',
+  },
+  body: {
+    flex: 5,
+    flexDirection: 'column',
+    // backgroundColor: 'green',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
+    margin: 10,
+  },
+  temperature: {
+    fontFamily: 'HelveticaNeue',
+    fontWeight: 'bold',
+    fontSize: 45,
+    color: 'white',
+  },
+  title: {
+    fontFamily: 'HelveticaNeue',
+    fontWeight: 'bold',
+    fontSize: 85,
+    color: 'white',
+  },
+  subtitle: {
+    fontSize: 30,
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
 
