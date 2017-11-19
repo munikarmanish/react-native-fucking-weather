3316498f887d58b93348bcd87fbba26fda271180
Manish Munikar <munikarmanish@gmail.com>
2017-11-19 13:10:33 +0545

    metric units

--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -2,12 +2,12 @@ const APPID = '19e6d91a4b43f822c7b772c6e573e59c';
 const rootUrl = 'http://api.openweathermap.org/data/2.5/weather';
 
 export const fetchWeather = (latitute, longigute) => {
-  const url = `${rootUrl}?apikey=${APPID}&lat=${latitute}&lon=${longigute}`;
+  const url = `${rootUrl}?apikey=${APPID}&lat=${latitute}&lon=${longigute}&units=metric`;
   console.log(url);
   return fetch(url)
     .then(res => res.json())
     .then(json => ({
-      temperature: json.main.temp,
+      temperature: Math.round(json.main.temp),
       weather: json.weather[0].main
     }));
 };
